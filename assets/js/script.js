bff2e442a731103c79829123649254091d77f2b4
Muhammad Abdullah <abdullahverse9176@gmail.com>
2025-11-07 20:00:40 +0500

    added home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,26 @@ $(".toggle-menu").click(function () {
 $(".close-mega-menu").click(function () {
     $(this).closest('.mega-menu-wrapper').find('.show-mega-menu').removeClass('show-mega-menu');
 })
+
+$('.arrival-slider').owlCarousel({
+    loop: true,
+    margin: 10,
+    center: true,
+    nav: true,
+    dots: false,
+    navText: [
+        '<img src="./assets/images/icons/prev.svg" alt="Prev" class="nav-arrow">',
+        '<img src="./assets/images/icons/next.svg" alt="Next" class="nav-arrow">'
+    ],
+    responsive: {
+        0: {
+            items: 1.7
+        },
+        767: {
+            items: 2.7
+        },
+        1200: {
+            items: 3.7
+        }
+    }
+})

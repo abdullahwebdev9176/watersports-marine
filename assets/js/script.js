1ca9a30d0fb0ff8cd60d08d9c2d82b886e1245c6
Muhammad Abdullah <abdullahverse9176@gmail.com>
2025-11-10 21:24:18 +0500

    updated header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,22 @@
 
 $(".toggle-menu").click(function () {
-    var menuTitle = $(this).attr('data-title');
-    var menuId = '#' + menuTitle;
+    var menuTitle = $(this).attr("data-title");
+    var menuId = "#" + menuTitle;
+    var $menu = $(menuId);
 
-    $('.mega-menu-wrapper').find('.show-mega-menu').removeClass('show-mega-menu');
-    $(menuId).addClass('show-mega-menu')
-})
+
+    if ($menu.hasClass("show-mega-menu")) {
+
+        $menu.removeClass("show-mega-menu");
+    } else {
+
+        $(".desktop-header")
+            .find(".show-mega-menu")
+            .removeClass("show-mega-menu");
+        $menu.addClass("show-mega-menu");
+    }
+});
+
 
 $(".close-mega-menu").click(function () {
     $(this).closest('.mega-menu-wrapper').find('.show-mega-menu').removeClass('show-mega-menu');
@@ -18,50 +29,44 @@ $('.footer-accordion-header').click(function () {
     $(this).find('.arrow-icon').toggleClass('rotate');
 });
 
-// $(".accordion-tab").on("click", function () {
-//     var e = "#" + $(this).attr("title");
-//     991 < $(window).width() ? ($(".service-accordion-content").find(".accordion-content-show").removeClass("accordion-content-show"),
-//         $(e).addClass("accordion-content-show")) : ($(this).after($(e).removeClass("accordion-content-show").toggle()),
-//             $(this).find("span.fa").toggleClass("rotate90"))
-// });
 
 $(".accordion-tab").on("click", function () {
-  var e = "#" + $(this).attr("title");
+    var e = "#" + $(this).attr("title");
 
-  if ($(window).width() > 991) {
-    // Desktop: Show only the selected content
-    $(".service-accordion-content .accordion-content-show")
-      .removeClass("accordion-content-show");
-    $(e).addClass("accordion-content-show");
-  } else {
-    // Mobile: Toggle accordion below clicked tab
-    $(this).next(".accordion-content").not(e).slideUp(); // close others
-    $(this).after($(e).stop(true, true).slideToggle());
-    $(this).find("span.fa").toggleClass("rotate90");
-  }
+    if ($(window).width() > 991) {
+
+        $(".service-accordion-content .accordion-content-show")
+            .removeClass("accordion-content-show");
+        $(e).addClass("accordion-content-show");
+    } else {
+
+        $(this).next(".accordion-content").not(e).slideUp();
+        $(this).after($(e).stop(true, true).slideToggle());
+        $(this).find("span.fa").toggleClass("rotate90");
+    }
 });
 
 
 
-    $('.arrival-slider').owlCarousel({
-        loop: true,
-        margin: 10,
-        center: true,
-        nav: true,
-        dots: false,
-        navText: [
-            '<img src="./assets/images/icons/prev.svg" alt="Prev" class="nav-arrow">',
-            '<img src="./assets/images/icons/next.svg" alt="Next" class="nav-arrow">'
-        ],
-        responsive: {
-            0: {
-                items: 1.7
-            },
-            767: {
-                items: 2.7
-            },
-            1200: {
-                items: 3.7
-            }
+$('.arrival-slider').owlCarousel({
+    loop: true,
+    margin: 10,
+    center: true,
+    nav: true,
+    dots: false,
+    navText: [
+        '<img src="./assets/images/icons/prev.svg" alt="Prev" class="nav-arrow">',
+        '<img src="./assets/images/icons/next.svg" alt="Next" class="nav-arrow">'
+    ],
+    responsive: {
+        0: {
+            items: 1.7
+        },
+        767: {
+            items: 2.7
+        },
+        1200: {
+            items: 3.7
         }
-    })
+    }
+})
